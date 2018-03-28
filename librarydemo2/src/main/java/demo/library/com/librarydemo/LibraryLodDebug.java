f3a5b2ee5bf34ed5bc0799cf2bc3467a189c067c
YudizShubham <shubham.s@yudiz.in>
2018-03-28 16:11:44 +0530

    init

---
+++ librarydemo2/src/main/java/demo/library/com/librarydemo/LibraryLodDebug.java
@@ -0,0 +1,14 @@
+package demo.library.com.librarydemo;
+
+import android.util.Log;
+
+/**
+ * Created by yudiz on 28/03/18.
+ */
+
+public class LibraryLodDebug {
+
+    public static void v(String message){
+        Log.v("LibraryDemo",message);
+    }
+}
